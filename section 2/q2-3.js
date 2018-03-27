9e7f2c543191c1e6389d06188848d163a5e92953
estines <estines.nop@gmail.com>
2018-03-27 14:18:24 +0700

    q2-3 answer

---
+++ section 2/q2-3.js
@@ -0,0 +1,22 @@
+const range = (init, end, condition = 1) => {
+    let Arr = []
+    if (init < end) {
+        for (i = init; i <= end; i = i + condition) {
+            Arr.push(i)
+        }
+    } else {
+        for (i = init; i >= end; i = i - condition) {
+            Arr.push(i)
+        }
+    }
+    return Arr
+}
+
+const sum = Arr => {
+    let result = 0
+    Arr.map(item => result += item)
+    return result
+}
+
+console.log(range(1,10,3))
+console.log(sum(range(15, 10)))
